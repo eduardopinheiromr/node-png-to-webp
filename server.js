90274d2f9a912415b391e7fdd87d44be599fab14
Eduardo Pinheiro <3dup1n@gmail.com>
2021-08-31 18:30:36 -0300

    fix: port fixed to prod server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fileupload = require("express-fileupload");
 
 const app = express();
-const port = 3000 | 3001;
+const port = process.env.PORT | 3000 | 3001;
 const baseUrl = "http://localhost:" + port;
 
 const convertToWebp = require("./src/services/convertToWebp");
